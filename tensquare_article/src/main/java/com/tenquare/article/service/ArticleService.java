54d1aefc91274cd42b9eb82f80d4d23b81e89810
EthanYu6 <1037916932@qq.com>
2019-10-26 10:39:31 +0800

    查询文章缓存操作

--- tensquare_article/src/main/java/com/tenquare/article/service/ArticleService.java
+++ tensquare_article/src/main/java/com/tenquare/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package com.tenquare.article.service;
 
 import com.tenquare.article.dao.ArticleDao;
+import com.tenquare.article.pojo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,10 @@ public class ArticleService {
 
     @Autowired
     private ArticleDao articleDao;
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
     /**
@@ -35,5 +41,22 @@ public class ArticleService {
     @Transactional
     public int updateThumbup(String id){
         return articleDao.updateThumbup(id);
+    }
+
+
+    /**
+     * 根据ID查询实体
+     * @param id
+     * @return
+     */
+    public Article findByID(String id){
+        //从缓存中提取
+        Article article = (Article) redisTemplate.opsForValue().get("article_" + id);
+        //如果缓存没有则到数据库查询并放入到缓存
+        if (article == null){
+            article = articleDao.findById(id).get();
+            redisTemplate.opsForValue().set("article_"+id,article);
+        }
+        return article;
     }
 }
