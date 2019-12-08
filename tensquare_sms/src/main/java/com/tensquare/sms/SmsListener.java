d77433d513395f3e83cab007f4e9374148f12228
EthanYu6 <1037916932@qq.com>
2019-12-08 16:27:52 +0800

    发送短信

--- tensquare_sms/src/main/java/com/tensquare/sms/SmsListener.java
+++ tensquare_sms/src/main/java/com/tensquare/sms/SmsListener.java
@@ -1,9 +1,13 @@
 package com.tensquare.sms;
 
+import com.aliyuncs.exceptions.ClientException;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 /**
@@ -16,6 +20,15 @@ import java.util.Map;
 @RabbitListener(queues = "sms")
 public class SmsListener {
 
+    @Autowired
+    private SmsUtil smsUtil;
+
+    @Value("${aliyun.sms.template_code}")
+    private String templateCode;//模板编号
+
+
+    @Value("${aliyun.sms.sign_name}")
+    private String signName;//签名
 
     /**
      * 发送短信
@@ -25,5 +38,11 @@ public class SmsListener {
     public void sendSms(Map<String,String> message){
         System.out.println("手机号:"+message.get("mobile"));
         System.out.println("验证码:"+message.get("code"));
+
+        try {
+            smsUtil.sendSms(message.get("monile"),templateCode,signName,"{\"code\":"+ message.get("code") +"}");
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
     }
 }
